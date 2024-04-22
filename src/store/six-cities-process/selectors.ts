6425af26964ddc512f5399524e017f3f57546ad0
Anton <anton.mogilev@mail.ru>
2024-04-22 21:52:53 +0300

    Добавит тесты для селекторов SixCitiesProcess

--- src/store/six-cities-process/selectors.ts
+++ src/store/six-cities-process/selectors.ts
@@ -2,5 +2,5 @@ import { NameSpace, TypesOfSorting } from '../../const';
 import { SixCities } from '../../types/sixCities';
 import { State } from '../../types/state';
 
-export const getCity = (state: State): SixCities => state[NameSpace.Sixities].city;
-export const getSortingType = (state: State): TypesOfSorting => state[NameSpace.Sixities].sortingType;
+export const getCity = (state: Pick<State, NameSpace.Sixities>): SixCities => state[NameSpace.Sixities].city;
+export const getSortingType = (state: Pick<State, NameSpace.Sixities>): TypesOfSorting => state[NameSpace.Sixities].sortingType;
