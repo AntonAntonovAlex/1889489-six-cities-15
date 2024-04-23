abc7125327b6adcfa2e36966f1c002a33a56ecca
Anton <anton.mogilev@mail.ru>
2024-04-23 21:28:38 +0300

    Добавит тесты для слайсов userProcess

---
+++ src/store/user-process/user.process.test.ts
@@ -0,0 +1,104 @@
+import { AuthorizationStatus } from '../../const';
+import { checkAuthAction, loginAction, logoutAction } from '../api-actions';
+import { userProcess } from './user-process';
+
+describe('UserProcess Slice', () => {
+  it('should return initial state with empty action', () => {
+    const emptyAction = { type: '' };
+    const expectedState = {
+      authorizationStatus: AuthorizationStatus.Auth,
+      userName: '',
+    };
+
+    const result = userProcess.reducer(expectedState, emptyAction);
+
+    expect(result).toEqual(expectedState);
+  });
+
+  it('should return default initial state with empty action', () => {
+    const emptyAction = { type: '' };
+    const expectedState = {
+      authorizationStatus: AuthorizationStatus.Unknown,
+      userName: '',
+    };
+
+    const result = userProcess.reducer(undefined, emptyAction);
+
+    expect(result).toEqual(expectedState);
+  });
+
+  it('should set "Auth" with "checkAuthAction.fulfilled" action', () => {
+    const initialState = {
+      authorizationStatus: AuthorizationStatus.NoAuth,
+      userName: '',
+    };
+    const expectedState = {
+      authorizationStatus: AuthorizationStatus.Auth,
+      userName: undefined,
+    };
+
+    const result = userProcess.reducer(initialState, checkAuthAction.fulfilled);
+
+    expect(result).toEqual(expectedState);
+  });
+
+  it('should set "NoAuth" with "checkAuthAction.rejected" action', () => {
+    const initialState = {
+      authorizationStatus: AuthorizationStatus.Auth,
+      userName: '',
+    };
+    const expectedState = {
+      authorizationStatus: AuthorizationStatus.NoAuth,
+      userName: '',
+    };
+
+    const result = userProcess.reducer(initialState, checkAuthAction.rejected);
+
+    expect(result).toEqual(expectedState);
+  });
+
+  it('should set "Auth" with "loginAction.fulfilled" action', () => {
+    const initialState = {
+      authorizationStatus: AuthorizationStatus.NoAuth,
+      userName: '',
+    };
+    const expectedState = {
+      authorizationStatus: AuthorizationStatus.Auth,
+      userName: undefined,
+    };
+
+    const result = userProcess.reducer(initialState, loginAction.fulfilled);
+
+    expect(result).toEqual(expectedState);
+  });
+
+  it('should set "NoAuth" with "loginAction.rejected" action', () => {
+    const initialState = {
+      authorizationStatus: AuthorizationStatus.Auth,
+      userName: '',
+    };
+    const expectedState = {
+      authorizationStatus: AuthorizationStatus.NoAuth,
+      userName: '',
+    };
+
+    const result = userProcess.reducer(initialState, loginAction.rejected);
+
+    expect(result).toEqual(expectedState);
+  });
+
+  it('should set "NoAuth", with "logoutAction.fulfilled" action', () => {
+    const initialState = {
+      authorizationStatus: AuthorizationStatus.Auth,
+      userName: '',
+    };
+    const expectedState = {
+      authorizationStatus: AuthorizationStatus.NoAuth,
+      userName: '',
+    };
+
+    const result = userProcess.reducer(initialState, logoutAction.fulfilled);
+
+    expect(result).toEqual(expectedState);
+  });
+});
